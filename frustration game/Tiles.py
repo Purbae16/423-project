988bc3ad2c13db905b2c029881070cb49d2b5417
Tazrian Hossain <144312101+Tazrian08@users.noreply.github.com>
2024-04-21 01:43:16 +0600

    Field 2 drawn

--- frustration game/Tiles.py
+++ frustration game/Tiles.py
@@ -1,16 +1,32 @@
 from OpenGL.GL import *
 from midpoint import Line
+import random
 
 
 class Tile:  # x0 is top left, x1 is top right, x2 is bottom left, x3 is bottom right
     def __init__(self, x0,y0):
 
-        self.height=18
-        self.width=18
+        self.height=30
+        self.width=30
+
+        glColor3f(1, 1, 1)
         Line(x0,y0,x0+self.width,y0)
         Line(x0,y0,x0,y0-self.height)
         Line(x0,y0-self.height,x0+self.width,y0-self.height)
         Line(x0+self.width,y0-self.height,x0+self.width,y0)
+
+class Greentile:
+    def __init__(self, x0,y0):
+
+        self.height=30
+        self.width=30
+
+        glColor3f(0.0, 0.8, 0.1)
+        Line(x0,y0,x0+self.width,y0)
+        Line(x0,y0,x0,y0-self.height)
+        Line(x0,y0-self.height,x0+self.width,y0-self.height)
+        Line(x0+self.width,y0-self.height,x0+self.width,y0)
+
 
 
 
